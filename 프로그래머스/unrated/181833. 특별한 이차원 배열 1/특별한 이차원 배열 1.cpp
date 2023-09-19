3bff8184e378f16c3ee4228641666d4b33dded84
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-19 13:05:17 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 1.30 ms, Memory: 4.69 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer(n);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            answer[i].emplace_back((int)(i == j));
+        }
+    }
+    
+    return answer;
+}

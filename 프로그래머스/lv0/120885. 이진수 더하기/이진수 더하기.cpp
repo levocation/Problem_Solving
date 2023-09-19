b4ec33e95bec0db22e5ce32d669ea437f0cd81c6
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-19 22:38:34 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.cpp
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -5,22 +5,33 @@
 using namespace std;
 
 string solution(string bin1, string bin2) {
+    
+    // 로직 요약
+    // bin1과 bin2를 10진수로 만들고
+    // 두 값을 더한 뒤에 2진수로 변환한다.
+    
     string answer = "";
     
     int s1 = 0;
     int s2 = 0;
     int sum = 0;
     
+    // bin1을 10진수 정수로 변환하는 작업
     for (int i = bin1.length() - 1, j = 1; i >= 0; i--, j *= 2) {
+        // 맨 뒷자리부터 2⁰, 2¹, 2², .... 2ⁿ을 해당 자리에 있는 숫자에 곱해서 s1에 더해준다.
         s1 += (bin1[i] - '0') * j;
     }
     
+    // bin2를 10진수 정수로 변환하는 작업
     for (int i = bin2.length() - 1, j = 1; i >= 0; i--, j *= 2) {
+        // 맨 뒷자리부터 2⁰, 2¹, 2², .... 2ⁿ을 해당 자리에 있는 숫자에 곱해서 s2에 더해준다.
         s2 += (bin2[i] - '0') * j;
     }
     
+    // bin1(10진수) + bin2(10진수)
     sum = s1 + s2;
     
+    // 2로 나눈 나머지를 맨 앞자리에 넣어준다.
     for (; sum != 0; sum = sum / 2) {
         answer = to_string(sum % 2) + answer;
     }

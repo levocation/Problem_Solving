e520cb97a2f73ed678ba244469e09a9c6d9197f0
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-19 14:21:16 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> arr) {
+    int answer = 1;
+    
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr.size(); j++) {
+            if (i == j) continue;
+            if (arr[i][j] != arr[j][i]) {
+                answer = 0;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}

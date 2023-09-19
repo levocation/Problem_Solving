31d328d67c9a9ce480527462ec3ac88a9b01fce2
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-19 15:22:44 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = n; i != 0; i /= 10) {
+        answer += (i % 10);
+    }
+    
+    return answer;
+}

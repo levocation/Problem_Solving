6afb56371a74ec37e02e03f2eaf0c591e91e7bb9
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-19 16:01:54 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    
+    for (int i = order; i != 0; i /= 10) {
+        if ((i % 10) == 3 
+            || (i % 10) == 6
+            || (i % 10) == 9) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}

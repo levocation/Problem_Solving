fbe44053de4e24fb51ed6166b953aae3e760b8d1
QUOTIENT <42001961+tjsgh5965@users.noreply.github.com>
2023-09-19 09:45:16 +0900

    [unrated] Title: 접미사 배열, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    for (int i = 0; i < my_string.size(); i++) {
+        answer.emplace_back(my_string.substr(i, 
+                                        my_string.size() - i));
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}

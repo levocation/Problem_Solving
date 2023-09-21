4b6e5029454939c4daaf895c486c552b99c7de99
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-21 14:57:24 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    
+    for (char ch : my_string) {
+        if (ch != letter[0]) answer += ch;
+    }
+    
+    return answer;
+}

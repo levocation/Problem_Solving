7820d5d88a0b5b93a2a5f4e41c64fbd425dc2eaa
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-09-21 15:11:34 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (answer = 1; (answer * 7) / n == 0; answer++) {}
+    
+    return answer;
+}

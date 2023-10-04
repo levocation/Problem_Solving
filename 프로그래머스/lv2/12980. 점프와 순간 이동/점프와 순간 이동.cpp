d2e5191da7d4910b62db01aaeb2b73af3f751301
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-10-04 21:57:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 1;
+    
+    while (n != 1) {
+        if (n % 2 == 1) {
+            n--;
+            ans++;
+        }
+        n /= 2;
+    }
+
+    return ans;
+}

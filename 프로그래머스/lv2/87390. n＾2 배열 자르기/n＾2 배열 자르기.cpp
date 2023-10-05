689a84a36e252b8183df66ad2712691287a7a118
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-10-05 14:26:59 +0900

    [level 2] Title: n^2 배열 자르기, Time: 32.75 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     
-    vector<int> answer;
+    vector<int> answer(right - left + 1, 0);
     
     for (long long i = 0; i < right - left + 1; i++) {
-        answer.emplace_back((i + left) / n > (i + left) % n 
+        answer[i] = ((i + left) / n > (i + left) % n 
                             ? (i + left) / n + 1 
                             : (i + left) % n + 1);
     }
-    
     return answer;
 }

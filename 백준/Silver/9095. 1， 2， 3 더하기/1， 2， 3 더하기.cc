57fb350c8308472961ef422710d8b6c0d7ea50c5
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-10-19 15:53:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+    int dp[11];
+    
+    int n = 0;
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    
+    for (int i = 4; i <= 11; i++) {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+    
+    int count = 0;
+    cin >> count;
+    
+    for (int i = 0; i < count; i++) {
+        cin >> n;
+    
+        cout << dp[n] << '\n';
+    }
+    
+    return 0;
+}

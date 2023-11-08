642fa0e37082e7094389e55012f8f1fc92788ddf
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-11-08 14:21:01 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -5,23 +5,26 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     long long a = 0, b = 0;
-    
+
     cin >> a >> b;
-    
+
     if (a > b) {
-        int temp = a;
+        long long temp = a;
         a = b;
         b = temp;
     }
-    
-    cout << (b - a - 1 < 0 ? 0 : b - a - 1) << '\n';
-    
-    
-    for (long long i = a + 1; i <= b - 1; i++) {
-        cout << i << ' ';
+
+    if (b - a <= 1) {
+        cout << "0\n";
     }
-    
+    else {
+        cout << b - a - 1 << "\n";
+        for (long long i = a + 1; i <= b - 1; i++) {
+            cout << i << ' ';
+        }
+    }
+
     return 0;
 }

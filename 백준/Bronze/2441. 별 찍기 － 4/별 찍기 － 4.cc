738a6f987821cb5a374f3eafe707f570e70623e7
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-11-08 17:00:52 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n = 0;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i > j) cout << ' ';
+            else cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}

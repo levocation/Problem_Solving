8de5d67f2d517b1d2f0a3de4face12ae20979879
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-11-09 10:48:24 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 10100 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <set>
 
 using namespace std;
 
@@ -7,25 +6,28 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int n[2000001] = {0,};
+    
     int tmp = 0, count = 0, answer = 0, target = 0;
     cin >> count;
     int *arr = new int[count];
-    set<int> s;
-
+    
     for (int i = 0; i < count; i++) {
         cin >> tmp;
         arr[i] = tmp;
-        s.emplace(tmp);
     }
     cin >> target;
-
+    
     for (int i = 0; i < count; i++) {
-        if (s.find(target - arr[i]) != s.end()) {
+        if (target - arr[i] < 0) continue;
+        if (n[target - arr[i]]) {
             answer++;
+        } else {
+            n[arr[i]] = 1;
         }
     }
-
-    cout << answer / 2;
-
+    
+    cout << answer;
+    
     return 0;
 }

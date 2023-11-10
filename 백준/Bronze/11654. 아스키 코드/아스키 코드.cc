e2a5045859a459aef3011feb76383f6780733fab
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-11-10 16:31:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char c;
+    cin >> c;
+    cout << (int)c;
+
+    return 0;
+}

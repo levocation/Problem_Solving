64f74701fd58f7a0765fd72c02f35f5aac7bce49
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-11-19 19:39:51 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long a, ans = 0;
+    
+    cin >> a;
+    
+    cout << a * (a - 1) / 2 << '\n' << 2;
+    
+    return 0;
+}

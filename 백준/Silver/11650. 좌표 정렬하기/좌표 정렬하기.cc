6e8a2f7a579e9cdef4e68e0b3d815a448bb539d7
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-11-30 14:48:36 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 52 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+#define X first
+#define Y second
+
+using namespace std;
+
+bool comp(pair<int, int> a, pair<int, int> b) {
+    if (a.X == b.X) return a.Y < b.Y;
+    return a.X < b.X;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    pair<int, int> tmp;
+    cin >> n;
+    vector<pair<int, int>> v;
+    
+    while (n--) {
+        cin >> tmp.X >> tmp.Y;
+        v.push_back(tmp);
+    }
+    
+    sort(v.begin(), v.end(), comp);
+    
+    for (pair<int, int> p : v) {
+        cout << p.X << ' ' << p.Y << '\n';
+    }
+    
+    return 0;
+}

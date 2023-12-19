5a106f7a03c0cce77c75cef22e78bb27dffce75f
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-12-19 09:54:07 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,25 +1,46 @@
 #include <iostream>
-#include <set>
+#include <algorithm>
 
+using ll = long long;
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, m, tmp;
-    set<int> s;
+    
+    ll n, m, tmp;
+    vector<ll> na;
+    vector<ll> ma;
+    
     cin >> n;
-    while (n--) {
+    for (ll i = 0; i < n; i++) {
         cin >> tmp;
-        s.insert(tmp);
+        na.push_back(tmp);
     }
     cin >> m;
-    while (m--) {
+    for (ll i = 0; i < m; i++) {
         cin >> tmp;
-        if (s.find(tmp) != s.end()) {
-            cout << "1\n";
-        } else {
-            cout << "0\n";
+        ma.push_back(tmp);
+    }
+    
+    sort(na.begin(), na.end());
+    
+    for (ll i = 0; i < m; i++) {
+        ll st = 0, en = (int)(na.size()) - 1;
+        
+        while (true) {
+            ll mid = (st + en) / 2;
+            if (na[mid] == ma[i]) {
+                cout << "1\n";
+                break;
+            } else if (st > en) {
+                cout << "0\n";
+                break;
+            }
+            if (na[mid] > ma[i]) en = mid - 1;
+            else st = mid + 1;
         }
     }
+    
+    return 0;
 }

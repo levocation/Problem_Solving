770a62349eee67ab71ba00477d0dbdbe11798f03
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-12-19 16:26:50 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, idx = 0, num = 0;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    vector<int> v;
+    vector<char> ans;
+    
+    while (true) {
+        if (idx >= n) {
+            for (char c : ans) cout << c << '\n';
+            break;
+        }
+        else if (num > n) {
+            cout << "NO";
+            break;
+        }
+        if (v.empty()) {
+            v.push_back(++num);
+            ans.push_back('+');
+        } else if (v.back() != arr[idx]) {
+            v.push_back(++num);
+            ans.push_back('+');
+        } else {
+            ans.push_back('-');
+            v.pop_back();
+            idx++;
+        }
+    }
+    
+    return 0;
+}

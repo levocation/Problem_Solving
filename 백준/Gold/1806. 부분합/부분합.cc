170b7d5cdace4896ccf5ee6027547ee5535fc9d5
QUOTIENT <42001961+levocation@users.noreply.github.com>
2023-12-21 09:28:28 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -6,31 +6,26 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, s, hap = 0, mx = 1000001;
+    int n, s;
     cin >> n >> s;
     int arr[n];
     for (int i = 0; i < n; i++) cin >> arr[i];
     
-    int st = 0, ed = 0;
-    hap = arr[0];
+    int sum = arr[0], st = 0, en = 0, ans = 2147483647;
     
-    while (ed < n) {
-        if (hap >= s) {
-            if (ed - st + 1 < mx) mx = ed - st + 1;
-            hap -= arr[st];
+    while (st <= en) {
+        if (sum >= s) {
+            if (en - st + 1 < ans) ans = en - st + 1;
+            sum -= arr[st];
             st++;
-            if (st > ed) {
-                ed = st;
-                hap += arr[ed];
-            }
         } else {
-            ed++;
-            if (ed >= n) break;
-            hap += arr[ed];
+            en++;
+            if (en >= n) break;
+            sum += arr[en];
         }
     }
     
-    cout << (mx == 1000001 ? 0 : mx);
+    cout << (ans == 2147483647 ? 0 : ans);
     
     return 0;
 }

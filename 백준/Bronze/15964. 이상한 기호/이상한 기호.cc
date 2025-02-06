042fe1b7e1b55611743dacf2609b1b7c97a64f8b
QUOTIENT <42001961+levocation@users.noreply.github.com>
2025-02-06 10:16:33 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll a, b;
+    cin >> a >> b;
+
+    cout << (a + b) * (a - b);
+
+    return 0;
+}

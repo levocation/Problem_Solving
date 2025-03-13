22d320de62f32e54cfc5ca1b3ea64fe6771b08c1
QUOTIENT <42001961+levocation@users.noreply.github.com>
2025-03-13 16:37:19 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 1; i < n; i++) {
+        v[i] += v[i - 1]; 
+    }
+
+    cout << accumulate(v.begin(), v.end(), 0);
+    
+    return 0;
+}

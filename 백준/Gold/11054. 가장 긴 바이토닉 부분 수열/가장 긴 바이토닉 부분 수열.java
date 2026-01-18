3e35d8553da0ba4f3517cf5760b25c0aa051759a
QUOTIENT <42001961+levocation@users.noreply.github.com>
2026-01-18 19:53:02 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 148 ms, Memory: 16436 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int answer = 1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        int[] dp2 = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+            dp2[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (arr[j] < arr[i]) dp2[i] = Math.max(dp2[i], dp2[j] + 1);
+            }
+            if (answer < dp[i] + dp2[i] - 1) answer = dp[i] + dp2[i] - 1;
+        }
+        
+        System.out.println(answer);
+    }
+}
